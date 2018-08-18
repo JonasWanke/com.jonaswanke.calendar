f30b2f5eb5f8c584ef2312b952f0a82ad5084d2b
Jonas Wanke <contact@jonas-wanke.com>
2018-08-18 16:47:32 +0200

    ui(calendar): add stroke to events for better distinction when stacking

--- calendar/src/main/java/com/jonaswanke/calendar/EventView.kt
+++ calendar/src/main/java/com/jonaswanke/calendar/EventView.kt
@@ -3,16 +3,16 @@ package com.jonaswanke.calendar
 import android.content.Context
 import android.graphics.Typeface
 import android.graphics.drawable.Drawable
-import androidx.annotation.AttrRes
-import androidx.annotation.StyleRes
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.drawable.DrawableCompat
-import androidx.appcompat.view.ContextThemeWrapper
+import android.graphics.drawable.GradientDrawable
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.StyleSpan
 import android.util.AttributeSet
 import android.widget.TextView
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
@@ -77,9 +77,7 @@ class EventView @JvmOverloads constructor(
             text = builder
         }
 
-        backgroundDrawable?.also {
-            DrawableCompat.setTint(it, event.color ?: backgroundColorDefault)
-        }
+        (backgroundDrawable as? GradientDrawable)?.setColor(event.color ?: backgroundColorDefault)
         background = backgroundDrawable
     }
 }
